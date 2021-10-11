0b274e0caa6d9960800a7f42a4a07771c82d8f49
Coding Forest <64945652+Coding-Forest@users.noreply.github.com>
2021-10-11 16:13:41 +1100

    Update MainActivity.java

--- 01 Features Code/Layout - Bottom Sheet/MainActivity.java
+++ 01 Features Code/Layout - Bottom Sheet/MainActivity.java
@@ -10,9 +10,15 @@ import android.widget.TextView;
 
 import com.google.android.material.bottomsheet.BottomSheetBehavior;
 
-public class MainActivity extends AppCompatActivity {
+/* In order to control BottomSheet, you need two elements:
+1. BottomSheet View,     (UI controller)
+2. BottomeSheetBehavior  (Activity controller)
 
-    private BottomSheetBehavior mBottomSheetBehavior;
+*/
+
+public class MainActivity extends AppCompatActivity {
+    
+    private BottomSheetBehavior BSBehavior;   // behaviour controller
     private TextView TVState;
 
     @Override
@@ -29,30 +35,34 @@ public class MainActivity extends AppCompatActivity {
         View bottomSheet = findViewById(R.id.bottom_sheet);
 
         // Instantiate the behavior from the view.
-        mBottomSheetBehavior = BottomSheetBehavior.from(bottomSheet);
+        BSBehavior = BottomSheetBehavior.from(bottomSheet);
 
         // Bottomsheet status
         TVState = findViewById(R.id.TVstate);
 
+        // When botton 'expand' is clicked, the state will be set to STATE_EXPANDED.
         BTexpand.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
+                BSBehavior.setState(BottomSheetBehavior.STATE_EXPANDED);
             }
         });
 
+        // When botton 'collapse' is clicked, the state will be set to STATE_COLLAPSED.
         BTcollapse.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                mBottomSheetBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
+                BSBehavior.setState(BottomSheetBehavior.STATE_COLLAPSED);
             }
         });
 
-        // Change the text of the bottomsheet state update TV.
-        mBottomSheetBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
+        // Update the text of the bottom sheet state TV.
+        // .setState(): Sets the state of the bottom sheet. The bottom sheet will transition to that state with animation.
+        BSBehavior.setBottomSheetCallback(new BottomSheetBehavior.BottomSheetCallback() {
             @Override
             public void onStateChanged(@NonNull View bottomSheet, int newState) {
                 switch (newState) {
+                        // all states here are static (completed, finished behaviours), hence represented in integers.
                     case BottomSheetBehavior.STATE_DRAGGING:
                         TVState.setText("Dragging");
                         break;
@@ -71,10 +81,18 @@ public class MainActivity extends AppCompatActivity {
                 }
 
             }
-
+    
+            // Unlike all the above static states, sliding is a dynamic 'process'. 
+            // Therefore, it is represented in float.
             @Override
             public void onSlide(@NonNull View bottomSheet, float slideOffset) {
                 TVState.setText("Sliding~");
+                
+                // Params - slideOffset:
+                // The new offset of this bottom sheet within [-1,1] range. 
+                // Offset increases as this bottom sheet is moving upward. 
+                // From 0 to 1 the sheet is between collapsed and expanded states and 
+                // from -1 to 0 it is between hidden and collapsed states.
             }
         });
     }
